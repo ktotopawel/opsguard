620bb371cdf0e624fae02ff2b2648c7cc904b7ef
ktotopawel <pawel.juszczak02@gmail.com>
2025-11-22 17:01:02 +0100

    initial commit

---
+++ src/main/java/com/ktotopawel/opsguard/OpsguardApplication.java
@@ -0,0 +1,13 @@
+package com.ktotopawel.opsguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsguardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpsguardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ktotopawel/opsguard/OpsguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktotopawel.opsguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
