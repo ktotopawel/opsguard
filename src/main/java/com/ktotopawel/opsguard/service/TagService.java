ddb1db9b4b59674e8708b46ca78991e157f7ae68
ktotopawel <pawel.juszczak02@gmail.com>
2025-11-29 19:33:39 +0100

    fix: unhandled race condition

--- src/main/java/com/ktotopawel/opsguard/service/TagService.java
+++ src/main/java/com/ktotopawel/opsguard/service/TagService.java
@@ -1,8 +1,10 @@
 package com.ktotopawel.opsguard.service;
 
 import com.ktotopawel.opsguard.entity.Tag;
+import com.ktotopawel.opsguard.exception.TagNotFoundException;
 import com.ktotopawel.opsguard.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +13,14 @@ public class TagService {
     private final TagRepository repository;
 
     public Tag findOrCreate(String name) {
-        return repository.findByName(name).orElseGet(() -> this.create(name));
+        try {
+            Tag newTag = new Tag();
+            newTag.setName(name);
+            return repository.save(newTag);
+        } catch (DataIntegrityViolationException e) {
+            // shouldn't throw, only enters this block if tag is already found in db
+            return repository.findByName(name).orElseThrow(() -> new TagNotFoundException("Failed to find tag with name " + name + "after trying to create it"));
+        }
     }
 
     public Tag create(String name) {
